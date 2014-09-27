0b6aa3f0039d51a9d6c7732b68e9dd565384d6d6
Alexey Efremov <alexey.efremov@saltpepper.ru>
2014-09-27 18:50:40 +0400

    fix codestyle

--- index.js
+++ index.js
@@ -10,45 +10,46 @@
 var postcss = require('postcss');
 var _ = require('lodash');
 
-function processor (options){
-	return function (css){
-		var nodes = [];
-		css.eachDecl(function(rule, data) {
-			if(rule.prop.match(/^background/) && rule._value.match(/url/) ){
-				var selector = "";
-				var selectors = rule.parent.selector.split(',');
-				for(var index in selectors){
-					if(!!selector){
-						selector += ", ";
-					}
-					selector += options.baseClass + " " + selectors[index].trim();
-				}
-				var rx = options.replace_from;
-				if(!_.isRegExp(options.replace_from)){
-					rx = new RegExp(rx);
-				}
-				var value = rule._value.replace(rx,options.replace_to);
-				var prop = rule.prop;
-				if( value.indexOf(",") === -1 && /url[ ]*\((.+)\)/g.exec(value) ){
-					value = "url(" + RegExp.$1 + ");";
-					prop = "background-image";
-				}
-				var new_rule = postcss.rule({selector:selector});
-				new_rule.append({
-					prop:prop,
-					value: value
-				});
-				nodes.push(new_rule);
-			}
-		});
-		for(var n=0; n < nodes.length; n++){
-			css.append(nodes[n]);
-		}
-	}
+function processor (_options){
+  var options = _.defaults(_options || {},{
+    baseClass: ".webp",
+    replace_from: /\.(png|jpg|jpeg)/,
+    replace_to: ".webp"
+  });
+  return function (css){
+    var nodes = [];
+    css.eachDecl(function(rule, data) {
+    if(rule.prop.indexOf("background") === 0 && rule._value.indexOf("url") >= 0 ){
+      var selector = "";
+      var selectors = rule.parent.selector.split(',');
+      for(var index in selectors){
+        if(!!selector){ selector += ", "; }
+        selector += options.baseClass + " " + selectors[index].trim();
+      }
+      var rx = options.replace_from;
+      if(!_.isRegExp(options.replace_from)){ rx = new RegExp(rx); }
+      var value = rule._value.replace(rx,options.replace_to);
+      var prop = rule.prop;
+      if( value.indexOf(",") === -1 && /url[ ]*\((.+)\)/g.exec(value) ){
+        value = "url(" + RegExp.$1 + ");";
+        prop = "background-image";
+      }
+      var new_rule = postcss.rule({selector:selector});
+      new_rule.append({
+        prop:prop,
+        value: value
+      });
+      nodes.push(new_rule);
+    }
+    });
+    for(var n=0; n < nodes.length; n++){
+      css.append(nodes[n]);
+    }
+  };
 }
 
 function transform (data, options){
-	return postcss(processor(options)).process(data).css;
+  return postcss(processor(options)).process(data).css;
 }
 
 module.exports.processor = processor;
