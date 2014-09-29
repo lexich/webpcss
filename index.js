fc3be9cb8ada49a30ac190c046e69c1885b3bc78
Alexey Efremov <alexey.efremov@saltpepper.ru>
2014-09-29 09:11:33 +0400

    refactoring

--- index.js
+++ index.js
@@ -7,11 +7,11 @@
  */
 'use strict';
 
-var postcss = require('postcss');
-var _ = require('lodash');
+var postcss = require("postcss");
+var _ = require("lodash");
 
 function processor (_options){
-  var options = _.defaults(_options || {},{
+  var options = _.defaults(_options || {}, {
     baseClass: ".webp",
     replace_from: /\.(png|jpg|jpeg)/,
     replace_to: ".webp"
@@ -20,35 +20,22 @@ function processor (_options){
     var nodes = [];
     css.eachDecl(function(decl, data) {
       if(decl.prop.indexOf("background") === 0 && decl.value.indexOf("url") >= 0 ){
-        var selector = "";
-        for(var index in decl.parent.selectors){
-          if(!!selector){ selector += ", "; }
-          selector += options.baseClass + " " + decl.parent.selectors[index].trim();
-        }
-        var rx = options.replace_from;
-        if(!_.isRegExp(options.replace_from)){ rx = new RegExp(rx); }
-        var value = decl.value.replace(rx,options.replace_to);
+        var selector = _.map(decl.parent.selectors, function(sel, i){
+          return options.baseClass + " " + sel;
+        }).join(", ");
+        var rx = _.isRegExp(options.replace_from) ? options.replace_from : new RegExp(rx);
+        var value = decl.value.replace(rx, options.replace_to);
         if(value === decl.value){ return; }
-        var prop = decl.prop;
         if( value.indexOf(",") === -1 && /url[ ]*\((.+)\)/g.exec(value) ){
           value = "url(" + RegExp.$1 + ")";
-          prop = "background-image";
-        }
+        } else { return; }
         var new_decl = decl.parent.clone({selector: selector});
-        new_decl.each(function (decl, i) {
-            new_decl.remove(i);
-        });
-        new_decl.append({
-          prop:prop,
-          value: value,
-          semicolon: true
-        });
+        new_decl.each(function (decl, i) { new_decl.remove(i); });
+        new_decl.append({prop: "background-image", value: value, semicolon: true});
         nodes.push(new_decl);
       }
     });
-    for(var n=0; n < nodes.length; n++){
-      css.append(nodes[n]);
-    }
+    nodes.forEach(function(decl){ css.append(decl); });
   };
 }
 
