b653c5ee179f8b4dc5473bccc6a45754b72f5270
Efremov Alexey <lexich121@gmail.com>
2014-10-20 13:36:37 +0400

    simple implement convert utility

--- test/base64_spec.js
+++ test/base64_spec.js
@@ -1,9 +1,14 @@
 var WebpBase64 = require("../lib/WebpBase64"),
-    base64stub = require("./fixtures/base64")
+    base64stub = require("./fixtures/base64"),
+    fs = require("fs"),
+    libpath = require("path"),
     should = require("should");
 
 describe("base64", function(){
   var base64 = new WebpBase64();
+  var pngbinary = fs.readFileSync(libpath.join(__dirname, "fixtures", "avatar.png" ));
+  var webp_pngbinary = fs.readFileSync(libpath.join(__dirname, "fixtures", "avatar.webp"));
+
   it("extract png", function(){
     var png = "data:image/png;base64,iVBORw";
     var url_png = "url(" + png + ")";
@@ -41,14 +46,11 @@ describe("base64", function(){
     );
   });
 
-  xit("extract webp data", function(finish){
-    var png = "data:image/png;base64,iVBORw";
-    base64.extract(png, false, function(err, data){
-      (true).should.be.ok;
-      console.log(err);
-      console.log("data");
-      console.log(data);
-      finish();
+  it("test convert data", function(next){
+    base64.convert({format: "png",data: pngbinary}, function(err, data){
+      (err === null).should.be.ok;
+      data.should.instanceof(Buffer);
+      next();
     });
     (true).should.be.ok;
   });
