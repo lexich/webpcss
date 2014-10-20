4a5e0ca90591e4bce4e14addba833c8c0b43db2b
Efremov Alexey <lexich121@gmail.com>
2014-10-20 10:40:55 +0400

    add url data module

---
+++ test/urldata_spec.js
@@ -0,0 +1,79 @@
+/*global describe, it*/
+"use strict";
+var urldata = require("../lib/urldata"),
+    should = require("should");
+
+describe("urldata", function(){
+  it("check urldata simple url", function(){
+    ["images/test.png"].should.eql(
+      urldata("url(images/test.png)")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url( images/test.png)")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url(images/test.png )")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url( images/test.png )")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url(  images/test.png  )")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url  (  images/test.png  ) ")
+    );
+  });
+  it("check urldata with single quote", function(){
+    ["images/test.png"].should.eql(
+      urldata("url('images/test.png')")
+    );
+    ["images/test.png"].should.eql(
+      urldata("url ( 'images/test.png' ) ")
+    );
+    [" images/test.png "].should.eql(
+      urldata("url ( ' images/test.png ' ) ")
+    );
+  });
+  it("check urldata with double quote", function(){
+    ["images/test.png"].should.eql(
+      urldata("url(\"images/test.png\")")
+    );
+  });
+  it("check urldata with wrong data", function(){
+    ["images/test.png\""].should.eql(
+      urldata("url(images/test.png\")")
+    );
+    ["\' images/test.png"].should.eql(
+      urldata("url( \' images/test.png)")
+    );
+  });
+  it("check multiple urls", function(){
+    ["cat.png", "dog.png" ].should.eql(
+      urldata("url(cat.png), url(dog.png)")
+    );
+    ["cat.png", "dog.gif", "mouse.jpg" ].should.eql(
+      urldata("url( cat.png), url(\'dog.gif\' ), url( \"mouse.jpg\" )")
+    );
+    ["cat.png", "dog.png" ].should.eql(
+      urldata("red url(cat.png) center bottom no-repeat, green url(dog.png) center bottom no-repeat")
+    );
+  });
+  it("check with data-uri", function(){
+    var png = "data:image/png;base64,iVBORw";
+    [png].should.eql(
+      urldata("url(" + png + ")")
+    );
+    [png, png+"a"].should.eql(
+      urldata("url(" + png + "), url(" + png + "a)")
+    );
+  });
+});
+
+
+
+
+
+
+
+
