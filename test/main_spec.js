b9459e830c00e05a238d3192a4282a7fe47a0774
Efremov Alexey <lexich121@gmail.com>
2015-09-18 10:34:03 +0300

    Remove: empty original rule

--- test/main_spec.js
+++ test/main_spec.js
@@ -20,55 +20,55 @@ describe("webpcss", function() {
   it("html tag", function() {
     var input = "html.test { background: url('test.png'); }";
     return webpcss.transform(input).then(function(res) {
-      expect("html.no-webp.test { background: url('test.png'); }" + "\nhtml.test { }" + "\nhtml.webp.test { background: url(test.webp); }").to.be.eql(res.css);
+      expect("html.no-webp.test { background: url('test.png'); }" + "\nhtml.webp.test { background: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("border-radius css property", function() {
     var input = ".test { border-image: url('test.png'); }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test { border-image: url('test.png'); }" + "\n.test { }" + "\n.webp .test { border-image: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { border-image: url('test.png'); }" + "\n.webp .test { border-image: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it(".html classname", function() {
     var input = ".html.test { background: url('test.png'); }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .html.test { background: url('test.png'); }" + "\n.html.test { }" + "\n.webp .html.test { background: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .html.test { background: url('test.png'); }" + "\n.webp .html.test { background: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("multiple selectors", function() {
     var input = ".test1, .test2 { background: url('test.png'); }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test1, .no-webp .test2 { background: url('test.png'); }" + "\n.test1, .test2 { }" + "\n.webp .test1, .webp .test2 { background: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test1, .no-webp .test2 { background: url('test.png'); }" + "\n.webp .test1, .webp .test2 { background: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("default options background-image with url", function() {
     var input = ".test { background-image: url(test.jpg); }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test { background-image: url(test.jpg); }" + "\n.test { }" + "\n.webp .test { background-image: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { background-image: url(test.jpg); }" + "\n.webp .test { background-image: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("default options background with url", function() {
     var input = ".test { background: url(test.jpeg); }";
     webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test { background: url(test.jpeg); }" + "\n.test { }" + "\n.webp .test { background: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { background: url(test.jpeg); }" + "\n.webp .test { background: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("default options background with url and params", function() {
     var input = ".test { background: transparent url(test.png) no-repeat; }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test { background: transparent url(test.png) no-repeat; }" + "\n.test { }" + "\n.webp .test { background: transparent url(test.webp) no-repeat; }").to.be.eql(res.css);
+      expect(".no-webp .test { background: transparent url(test.png) no-repeat; }" + "\n.webp .test { background: transparent url(test.webp) no-repeat; }").to.be.eql(res.css);
     });
   });
 
   it("default options background multiple urls", function() {
     var input = ".img_play_photo_multiple{ background: url(number.png) 600px 10px no-repeat,\nurl(\"thingy.png\") 10px 10px no-repeat,\nurl('Paper-4.png');\n}";
-    var output = ".no-webp .img_play_photo_multiple{ background: url(number.png) 600px 10px no-repeat,\nurl(\"thingy.png\") 10px 10px no-repeat,\nurl('Paper-4.png'); }" + "\n.img_play_photo_multiple{\n}" + "\n.webp .img_play_photo_multiple{ background: url(number.webp) 600px 10px no-repeat,\nurl(thingy.webp) 10px 10px no-repeat,\nurl(Paper-4.webp); }";
+    var output = ".no-webp .img_play_photo_multiple { background: url(number.png) 600px 10px no-repeat,\nurl(\"thingy.png\") 10px 10px no-repeat,\nurl('Paper-4.png'); }" + "\n.webp .img_play_photo_multiple { background: url(number.webp) 600px 10px no-repeat,\nurl(thingy.webp) 10px 10px no-repeat,\nurl(Paper-4.webp); }";
     return webpcss.transform(input).then(function(res) {
       expect(output).to.be.eql(res.css);
     });
@@ -77,8 +77,8 @@ describe("webpcss", function() {
   it("default options multiple mixed clasess", function() {
     var input = ".test1{ background: url(\"test1.jpeg\");}" +
         ".test2{ background-image: url(\'test2.png\');}";
-    var output = ".no-webp .test1{ background: url(\"test1.jpeg\"); }" + ".test1{}" +
-      ".no-webp .test2{ background-image: url('test2.png'); }" + ".test2{}" + ".webp .test1{ background: url(test1.webp); }" + ".webp .test2{ background-image: url(test2.webp); }";
+    var output = ".no-webp .test1 { background: url(\"test1.jpeg\"); }" +
+      ".no-webp .test2 { background-image: url('test2.png'); }" + ".webp .test1 { background: url(test1.webp); }" + ".webp .test2 { background-image: url(test2.webp); }";
 
     return webpcss.transform(input).then(function(res) {
       expect(output).to.be.eql(res.css);
@@ -96,7 +96,7 @@ describe("webpcss", function() {
   it("default options background with gif and jpg", function() {
     var input = ".test { background: url(test.gif), url(\"test1.jpg\"); }";
     return webpcss.transform(input).then(function(res) {
-      expect(".no-webp .test { background: url(test.gif), url(\"test1.jpg\"); }" + "\n.test { }" + "\n.webp .test { background: url(test.gif), url(test1.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { background: url(test.gif), url(\"test1.jpg\"); }" + "\n.webp .test { background: url(test.gif), url(test1.webp); }").to.be.eql(res.css);
     });
   });
 
@@ -110,27 +110,27 @@ describe("webpcss", function() {
   it("custom options webpClass", function() {
     var input = ".test { background-image: url(test.png); }";
     return webpcss.transform(input, {webpClass: ".webp1"}).then(function(res) {
-      expect(".no-webp .test { background-image: url(test.png); }" + "\n.test { }" + "\n.webp1 .test { background-image: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { background-image: url(test.png); }" + "\n.webp1 .test { background-image: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("custom options replace_from background with gif", function() {
     var input = ".test { background: url(test.gif); }";
     return webpcss.transform(input, {replace_from: /\.gif/g}).then(function(res) {
-      expect(".no-webp .test { background: url(test.gif); }" + "\n.test { }" + "\n.webp .test { background: url(test.webp); }").to.be.eql(res.css);
+      expect(".no-webp .test { background: url(test.gif); }" + "\n.webp .test { background: url(test.webp); }").to.be.eql(res.css);
     });
   });
 
   it("custom options replace_to background-image with url", function() {
     var input = ".test { background-image: url(test.jpg); }";
     return webpcss.transform(input, {replace_to: ".other"}).then(function(res) {
-      expect(".no-webp .test { background-image: url(test.jpg); }" + "\n.test { }" + "\n.webp .test { background-image: url(test.other); }").to.be.eql(res.css);
+      expect(".no-webp .test { background-image: url(test.jpg); }" + "\n.webp .test { background-image: url(test.other); }").to.be.eql(res.css);
     });
   });
 
   it("check with @media-query", function() {
     var input = "@media all and (min-width:100px){ .test { background-image: url(test.jpg); } }";
-    var output = "@media all and (min-width:100px){ .no-webp .test{ background-image: url(test.jpg); } .test { } }" + " @media all and (min-width:100px){ .webp .test{ background-image: url(test.webp); } }";
+    var output = "@media all and (min-width:100px){ .no-webp .test{ background-image: url(test.jpg); } }" + " @media all and (min-width:100px){ .webp .test{ background-image: url(test.webp); } }";
     return webpcss.transform(input).then(function(res) {
       expect(output).to.be.eql(res.css);
     });
@@ -138,7 +138,7 @@ describe("webpcss", function() {
 
   it("check with multiple @media-query", function() {
     var input = "@media all and (max-width:200px){ @media all and (min-width:100px){ .test { background-image: url(test.jpg); } } }";
-    var output = "@media all and (max-width:200px){ @media all and (min-width:100px){ .no-webp .test { background-image: url(test.jpg); } } }" + "@media all and (max-width:200px){ @media all and (min-width:100px){ .test { } } }" + " @media all and (max-width:200px){ @media all and (min-width:100px){ .webp .test{ background-image: url(test.webp); } } }";
+    var output = "@media all and (max-width:200px){ @media all and (min-width:100px){ .no-webp .test { background-image: url(test.jpg); } } }" + " @media all and (max-width:200px){ @media all and (min-width:100px){ .webp .test{ background-image: url(test.webp); } } }";
     webpcss.transform(input).then(function(res) {
       expect(output).to.be.eql(res.css);
     });
@@ -164,7 +164,7 @@ describe("webpcss", function() {
       expect(css).to.match(/data:image\/png;base64,/);
       expect(css).to.match(/\.no-webp \.test { background: url\(data:image\/png;base64,/);
 
-      expect(css).to.match(/\.test { }/);
+      expect(css).to.not.match(/\.test { }/);
 
       expect(css).to.match(/data:image\/webp;base64,/);
       expect(css).to.match(/\.webp \.test { background: url\(data:image\/webp;base64,/);
@@ -178,7 +178,7 @@ describe("webpcss", function() {
       expect(css).to.match(/data:image\/jpg;base64,/);
       expect(css).to.match(/\.no-webp \.test { background: url\(data:image\/jpg;base64,/);
 
-      expect(css).to.match(/\.test { }/);
+      expect(css).to.not.match(/\.test { }/);
 
       expect(css).to.match(/data:image\/webp;base64,/);
       expect(css).to.match(/\.webp \.test { background: url\(data:image\/webp;base64,/);
